e55f0a9ac2d5c4e2e51e488b0d0a3ab129c93211
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:10:07 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs7.js
@@ -0,0 +1,9 @@
+function tugas7() {
+    const array = [162, 170, 158, 184, 155, 160, 177, 174, 169, 160];
+    for (const iterator of array) {
+        console.log(iterator);
+    }
+}
+tugas7();
+//perbedaanya adalah dengan menggunakan for of codingan menjadi lebih bersih dengan menggunakan sintax variable of iteration
+//dimana variable of iteration ini akan mengambil data dari array yang telah di deklarasikan sebelumnya
